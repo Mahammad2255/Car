5ce99aa429642ca7ab5f96ea6ced1976a9f99efe
Mahammad2255 <[mahammadei@code.edu.az]>
2021-12-19 11:23:48 -0800

    Add project files.

---
+++ Ttapsiriq1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ttapsiriq1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Drive();
+        }
+    }
+}
